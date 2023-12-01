e0bc85bdac6bd17de0bb0957ee7e339758e1d01d
DarkM1nd <125229600+VZNURUIM@users.noreply.github.com>
2023-12-01 16:15:12 -0300

    Create Radian_to_degrees_in_CPP.cpp

---
+++ Radian_to_degrees_in_CPP.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+double radiano(double r) {
+	return r * 57.2958;
+}
+
+int main() {
+	double number_radiano{};
+
+	cout << "Digite o radiano: ";
+	cin >> number_radiano;
+
+	cout << "Radiano para graus: " << radiano(number_radiano);
+}
